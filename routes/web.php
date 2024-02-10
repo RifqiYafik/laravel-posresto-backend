df69149a9ce8d74dfd6aa1c7dfa41f0c3a802fa5
Rifqi Yafik <rifqiyafika50@gmail.com>
2024-02-10 10:11:59 +0700

    init resto backend

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.auth.login1');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function() {
+        return view('pages.dashboard');
+    })->name('home');
+    Route::resource('users', UserController::class);
+});
+
+// Route::get('/login', function () {
+//     return view('pages.auth.login');
+// });
+
